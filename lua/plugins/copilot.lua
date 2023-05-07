9c4c407f4e99488d87266bd56ae885ca0add423c
sdoering01 <57443123+sdoering01@users.noreply.github.com>
2023-05-07 17:51:25 +0200

    fix github copilot inserting extra characters

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,4 +1,4 @@
-vim.keymap.set('i', '<C-j>', 'copilot#Accept("<CR>")', { silent = true, expr = true })
+vim.keymap.set('i', '<C-j>', 'copilot#Accept("<CR>")', { silent = true, expr = true, replace_keycodes = false })
 vim.g.copilot_no_tab_completion = true
 vim.g.copilot_filetypes = {
     vimwiki = false,
